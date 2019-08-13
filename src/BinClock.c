d8695f43c36e335e15e128ce97a7b2aa2115d4cd
Akha Manjezi <amanjezi@gmail.com>
2019-08-13 17:52:08 +0200

    added names and date

--- src/BinClock.c
+++ src/BinClock.c
@@ -4,8 +4,8 @@
  * Modified for EEE3095S/3096S by Keegan Crankshaw
  * August 2019
  * 
- * <STUDNUM_1> <STUDNUM_2>
- * Date
+ * GEOJNW001 MNJSIN002
+ * Date: 20 August 2019
 */
 
 #include <wiringPi.h>
